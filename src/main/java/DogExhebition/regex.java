0745addb77da2fe4e5992262ee68faff6bbca414
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-05-30 23:38:17 +0300

    added testRegex file

--- src/main/java/DogExhebition/regex.java
+++ src/main/java/DogExhebition/regex.java
@@ -6,9 +6,15 @@ import java.util.regex.Pattern;
 public class regex {
     
     public static void main(String [] args){
-        String regex = "^[А-Яа-я]+$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher("Андрей");
-        System.out.println("Андрей" +" : "+ matcher.matches());
+        String regex_person_name = "^[А-Я]{1}[а-я]*( ){1}[А-Я]{1}[а-я]*(\\-[А-Я]{1}[а-я]*)?( {1}[0-9]+)?$";
+        Pattern pattern_person_name = Pattern.compile(regex_person_name);
+        String examples[] = {"Андрей Богодеев", "Андрей  Догодеев", "Андрей Богодеев-Деев", "Андрей БЛогодеев", "Андрей Богодеев 8", "Андрей Богодеев-Деев 8", "Андрей", "   ", "---", "-", "---///"};
+        int i =0;
+        for (String ex : examples){
+            Matcher matcher = pattern_person_name.matcher(ex);
+            System.out.println((i+1)+" "+ex +" : "+ matcher.matches());
+            i++;
+        }
+        
     }
 }
