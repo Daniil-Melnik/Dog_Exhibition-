cc4d44cb9de59ad96bdc4258a2b4cca8dbd8ae73
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-06-07 14:53:15 +0300

    add communication class for judges with breeds

---
+++ src/main/java/DogExhebition/J_B_com.java
@@ -0,0 +1,24 @@
+package DogExhebition;
+
+public class J_B_com {
+    private Judge judge;
+    private Breed breed;
+
+    public J_B_com(){
+
+    }
+
+    public void setJudge(Judge judge){
+        this.judge = judge;
+    }
+    public Judge getJudge(){
+        return this.judge;
+    }
+
+    public void setBreed(Breed breed){
+        this.breed = breed;
+    }
+    public Breed getBreed(){
+        return this.breed;
+    }
+}
